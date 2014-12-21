2cfe661cea1914a8212da9450f0a0fad27d97bcd
Collin Stedman <cstedman@princeton>
2014-12-21 13:14:01 -0600

    CAS login works

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express'),
     app = express();
 
-var bodyParser = require('body-parser');
-var compress = require('compression');
+var bodyParser = require('body-parser'),
+    compress   = require('compression'),
+    session    = require('cookie-session');
+
 
 var CAS = require('cas');
 var cas_url = 'https://fed.princeton.edu/cas/';
-var cas = new CAS({ base_url: cas_url, service: 'http://localhost:8181/test' });
+var cas = new CAS({ base_url: cas_url, service: 'http://localhost:8181/verify' });
 
 var port = process.argv[2] || 8181;
 
@@ -16,31 +18,48 @@ var staticFileOptions = {
 };
 
 app.use(compress())
-   .use(bodyParser.urlencoded({ extended: true }));
+   .use(bodyParser.urlencoded({ extended: true }))
+   .use(session({ keys: ['key1', 'key2'] }));
 
 app.get('/', function(req, res) {
-  res.redirect(cas_url + 'login?service=http://localhost:8181/test');
+  if (req.session.cas) {
+    res.render('index.jade', req.session.cas);
+  }
+  else {
+    res.sendFile('index.html', staticFileOptions);
+  }
 });
 
-app.get('/test', function(req, res) {
-  var ticket = req.param('ticket');
-  if (ticket) {
-    cas.validate(ticket, function(err, status, username) {
-      if (err) {
-        res.send({ error: err });
-      }
-      else {
-        res.send({ status: status, username: username });
-      }
-    });
+app.get('/login', function(req, res) {
+  res.redirect(cas_url + 'login?service=http://localhost:8181/verify');
+});
+
+app.get('/verify', function(req, res) {
+  if (!req.session.cas) {
+    var ticket = req.param('ticket');
+    if (ticket) {
+      cas.validate(ticket, function(err, status, username) {
+        if (err) {
+          res.send({ error: err });
+        }
+        else {
+          req.session.cas = { age: 'new', status: status, username: username };
+          res.redirect('/');
+        }
+      });
+    }
+    else {
+      res.redirect('/');
+    }
   }
   else {
     res.redirect('/');
   }
 });
 
-app.post('/login', function(req, res) {
-  console.log(req.body);
+app.get('/logout', function(req, res) {
+  req.session = null;
+  res.redirect('https://fed.princeton.edu/cas/logout');
 });
 
 app.use(function(req, res, next) {
